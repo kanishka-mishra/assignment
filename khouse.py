1d263ea5ec0d5fea3deb78f88a8cd639870f1f4b
kanishka-mishra <drkanishkamishra@gmail.com>
2024-09-19 18:33:31 +0530

    graphics
    this is a my python program

---
+++ khouse.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 17:32:53 2024
+
+@author: ASUS
+"""
+
+import turtle
+t=turtle.Turtle()
+
+t.forward(140)
+t.left(120)
+t.forward(130)
+t.left(120)
+t.forward(130)
+t.left(30)
+t.forward(100)
+t.left(90)
+t.forward(130)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(110)
+t.left(90)
+t.forward(155)
+t.penup()
+t.left(70)
+t.forward(60)
+t.pendown()
+t.circle(15)
