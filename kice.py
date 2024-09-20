b75a16a5d1b37f044e3ee926590a55be3490480b
kanishka-mishra <drkanishkamishra@gmail.com>
2024-09-20 07:56:29 +0530

    turtle
    python file

---
+++ kice.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 18:03:42 2024
+
+@author: ASUS
+"""
+
+import turtle
+t=turtle.Turtle()
+t.left(90)
+t.circle(100,180)
+t.left(90)
+t.forward(200)
+t.right(120)
+t.forward(200)
+t.right(120)
+t.forward(200)
+t.backward(170)
+t.right(90)
+t.forward(50)
+t.left(140)
+t.forward(70)
+t.right(150)
+t.forward(100)
+t.left(150)
+t.forward(120)
+t.right(152)
+t.forward(168)
---
+++ kqube.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 18:32:04 2024
+
+@author: ASUS
+"""
+
+import turtle
+t=turtle.Turtle()
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(130)
+t.forward(100)
+t.right(40)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.right(140)
+t.forward(100)
+t.penup()
+t.right(130)
+t.forward(200)
+t.pendown()
+t.right(50)
+t.forward(100)
+t.penup()
+t.left(140)
+t.forward(200)
+t.pendown()
+t.left(40)
+t.forward(100)
+
+
+
+
+
+
+
+
+
+
+
