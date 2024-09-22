6a176733bc793c8d5d863a73f723f1c7e53e4989
kanishka-mishra <drkanishkamishra@gmail.com>
2024-09-22 22:07:08 +0530

    turtle

---
+++ circle.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 18:21:15 2024
+
+@author: ASUS
+"""
+
+#WAP to make circle with the help of sequrq 
+
+import turtle
+t=turtle.Turtle()
+t.speed(0.5)
+for i in range(40) :
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(130)
+    
